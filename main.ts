44c192637c3cfa5db0bef9833cfc246bbe66dd19
tokadev64 <tokada.dev0@gmail.com>
2024-05-12 00:39:02 +0900

    add wgrib2 api

--- main.ts
+++ main.ts
@@ -1,9 +1,19 @@
+// import { CommandBuilder } from "https://deno.land/x/dax@0.31.1/src/command.ts";
+import $ from "@david/dax";
+
 import { Hono } from "hono";
 
 const app = new Hono();
 
+$.setPrintCommand(true);
+
 app.get("/get", (c) => c.json({ message: "Hello Deno!" }));
 app.post("/post", (c) => c.req.json().then((json) => c.json(json)));
+
+app.post("/wgrib2", async (c) => {
+	const result = await $`wgrib2 | grep ''`.text();
+	return c.text(result);
+});
 
 Deno.serve({ port: 1993 }, app.fetch);
 app.fire();

2d19eb99702726868a55e46262d72daff08649ea
“rsq-okada” <t.okada@rescuenow.co.jp>
2024-05-30 18:43:05 +0900

    fix api wgrib2 option

--- main.ts
+++ main.ts
@@ -1,22 +1,18 @@
+import $ from "@david/dax";
 import { Hono } from 'hono';
 
 const app = new Hono();
+$.setPrintCommand(true);
 
 app.get('/get', (c) => c.json({ message: 'Hello Deno!' }));
 app.post('/post', (c) => c.req.json().then((json) => c.json(json)));
 
 app.post('/wgrib2', async (c) => {
 	const json = await c.req.json();
-
-	const command = new Deno.Command('wgrib2', {
-		args: [json.option, json.url].filter(Boolean)
-	});
-	const { success, code, stdout, stderr } = await command.outputSync();
-	console.assert(success);
-
-	return (code === 0)
-		? c.text(new TextDecoder().decode(stdout))
-		: c.text(new TextDecoder().decode(stderr));
+	const path = [json.dir, json.filepath].join('');
+	const args = [json.option, path].filter(Boolean).join(' ');
+	const result = await $.raw`wgrib2 ${args}`.text();
+	return c.text(result);
 });
 
 Deno.serve({ port: 1993 }, app.fetch);

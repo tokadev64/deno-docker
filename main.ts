0d256bc6a11c5aee8d2a6dec5922c8cca522e2df
“rsq-okada” <t.okada@rescuenow.co.jp>
2024-05-30 16:05:53 +0900

    add Dockerfile for Arm

--- main.ts
+++ main.ts
@@ -1,16 +1,15 @@
-// import { CommandBuilder } from "https://deno.land/x/dax@0.31.1/src/command.ts";
 import $ from "@david/dax";
-
 import { Hono } from "hono";
 
 const app = new Hono();
-
 $.setPrintCommand(true);
 
 app.get("/get", (c) => c.json({ message: "Hello Deno!" }));
 app.post("/post", (c) => c.req.json().then((json) => c.json(json)));
 
 app.post("/wgrib2", async (c) => {
+	const post = await c.req.json();
+	console.info('/wgrib2 post', post);
 	const result = await $`wgrib2 | grep ''`.text();
 	return c.text(result);
 });

7ab09db90e50793fb66d08ff296c4b2c4dc3789d
“rsq-okada” <t.okada@rescuenow.co.jp>
2024-05-30 17:39:26 +0900

    fix api wgrib2

--- main.ts
+++ main.ts
@@ -1,17 +1,22 @@
-import $ from "@david/dax";
-import { Hono } from "hono";
+import { Hono } from 'hono';
 
 const app = new Hono();
-$.setPrintCommand(true);
 
-app.get("/get", (c) => c.json({ message: "Hello Deno!" }));
-app.post("/post", (c) => c.req.json().then((json) => c.json(json)));
+app.get('/get', (c) => c.json({ message: 'Hello Deno!' }));
+app.post('/post', (c) => c.req.json().then((json) => c.json(json)));
 
-app.post("/wgrib2", async (c) => {
-	const post = await c.req.json();
-	console.info('/wgrib2 post', post);
-	const result = await $`wgrib2 | grep ''`.text();
-	return c.text(result);
+app.post('/wgrib2', async (c) => {
+	const json = await c.req.json();
+
+	const command = new Deno.Command('wgrib2', {
+		args: [json.option, json.url].filter(Boolean)
+	});
+	const { success, code, stdout, stderr } = await command.outputSync();
+	console.assert(success);
+
+	return (code === 0)
+		? c.text(new TextDecoder().decode(stdout))
+		: c.text(new TextDecoder().decode(stderr));
 });
 
 Deno.serve({ port: 1993 }, app.fetch);

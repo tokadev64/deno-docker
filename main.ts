52eb8563a850126ba64896a10991de254b6c4189
“rsq-okada” <t.okada@rescuenow.co.jp>
2024-05-31 14:47:22 +0900

    add const exec cmd wgrib2

--- main.ts
+++ main.ts
@@ -8,12 +8,19 @@ app.get('/get', (c) => c.json({ message: 'Hello Deno!' }));
 app.post('/post', (c) => c.req.json().then((json) => c.json(json)));
 
 app.post('/wgrib2', async (c) => {
-	const json = await c.req.json();
-	const path = [json.dir, json.filepath].join('');
-	const args = [json.option, path].filter(Boolean).join(' ');
-	const result = await $.raw`wgrib2 ${args}`.text();
+	const result = await execCommandWgrib2(await c.req.json());
 	return c.text(result);
 });
 
 Deno.serve({ port: 1993 }, app.fetch);
 app.fire();
+
+const execCommandWgrib2 = async (json: {
+	option: string;
+	dir: string;
+	filepath: string;
+}): Promise<string> => {
+	const path = [json.dir, json.filepath].join('');
+	const args = [json.option, path].filter(Boolean).join(' ');
+	return await $.raw`wgrib2 ${args}`.text();
+};
